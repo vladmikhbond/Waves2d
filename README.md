d56b4e04299f628b8a2243d7531d81b46e02b551
vladmikhbond <vladmikhbond@gmail.com>
2026-05-02 10:31:47 +0300

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# VawesTS
+
+TypeScript application template.
+
+## Commands
+
+```bash
+npm install    # Install dependencies
+npm run build  # Compile TypeScript
+npm start      # Run compiled app
+npm run dev    # Run with ts-node
+```
---
+++ src/models/space.ts
@@ -0,0 +1,81 @@
+class Node {
+    z: number = 0
+    v: number  = 0
+}
+
+class Oscillator {
+    a = 0    
+    t = 0
+    r = 0
+    c = 0
+    dt = 0.1
+    
+    constructor(r: number, c: number, a: number) {
+        this.r = r;
+        this.c = c;
+        this.a = a;
+    }
+    
+    next_z() {
+        this.t += this.dt ;
+        return Math.sin(this.t) * this.a;
+    }
+}
+
+
+export default class Space {
+    k = 0  // жорсткість
+    m = 0  // маса
+    time = 0  // такти часу
+    loss = 0.99  // коеф. втрат
+    nodes: Node[][] = []
+    oscillators: Oscillator[] = []
+
+
+    constructor(n: number, k: number, m: number, l: number) {
+        this.k = k;
+        this.m = m;
+        this.loss = l;
+
+        this.nodes = new Array(n);
+        for (let i = 0; i < n; i++) {
+            this.nodes[i] = new Array(n);
+            for (let j = 0; j < n; j++) {
+                this.nodes[i][j] = new Node();
+            }
+        }
+        
+        // осцилятори
+        this.oscillators.push(new Oscillator(n/2, n/2, 1));
+    }
+
+    step() {
+        let n = this.nodes.length;
+        // швидкості
+        for (let r = 1; r < n - 1; r++) {
+            for (let c = 1; c < n - 1; c++) {
+                let dz = this.nodes[r-1][c-1].z + this.nodes[r-1][c+1].z +
+                         this.nodes[r+1][c-1].z + this.nodes[r+1][c+1].z -
+                         4 * this.nodes[r][c].z;
+                let a = (this.k / this.m) * dz;
+                this.nodes[r][c].v += a;
+                this.nodes[r][c].v *= this.loss;
+            }
+        }
+        // амплітуди
+        for (let r = 1; r < n - 1; r++) {
+            for (let c = 1; c < n - 1; c++) {
+                this.nodes[r][c].z += this.nodes[r][c].v;
+            }
+        }
+
+        // осцилятори
+        for (let o of this.oscillators) {
+            this.nodes[o.r][o.c].z = o.next_z();
+        }
+    
+
+        this.time++;
+    }
+
+}
